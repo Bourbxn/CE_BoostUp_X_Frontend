4c3396b63502915a2d5725d95eed9e3e86e9166e
OOT OOT <ootcazy007@gmail.com>
2022-06-12 02:04:32 +0700

    90% (fetch api,fix last page,improve)

--- my-app/src/pages/Ranking.jsx
+++ my-app/src/pages/Ranking.jsx
@@ -11,100 +11,114 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import {faCircleArrowRight} from '@fortawesome/free-solid-svg-icons'
 
-let curPage = 1;
-
-const randPlayer=(i)=>{
-    return{
-        rank : i,
-        playerName : "Playername",
-        home : Math.floor(Math.random()*6),
-        point : Math.floor(Math.random()*10000)
-    }
-}
-
-const range = (len) =>{
-    const arr=[];
-    for(let i=0;i<len;i++){
-        arr.push(i);
-        arr[i] = randPlayer(i+1);
-    }
-    return arr;
-}
-
-const scoreBoard = (curPage,arr) =>{
-    const borad=[];
-    const len = curPage*10;
-    const start = len-10;
-    let count=0;
-    for(let i = start; i <len; i++ ){
-        borad[count]=arr[i];
-        count++;
-    }
-    return borad;
-}
-
-const Home = (home) => {
-    switch(home){
-        case 0:
-            return (
-                <div className="flex_center">
-                    <img src={jupiter} alt="Jupiter" className="home_img"/>
-                </div>
-            );
-        case 1:
-            return (
-                <div className="flex_center">
-                        <img src={mars} alt="Mars" className="home_img"/>
-                </div>
-            );
-        case 2:
-            return (
-                <div className="flex_center">
-                    <img src={mercury} alt="Mercury" className="home_img"/>
-                </div>
-            );
-        case 3:
-            return (
-                <div className="flex_center">
-                    <img src={naptune} alt="Naptune" className="home_img"/>
-                </div>
-            );
-        case 4:
-            return (
-                <div className="flex_center">
-                    <img src={pluto} alt="Pluto" className="home_img"/>
-                </div>
-            );
-        case 5:
-            return (
-                <div className="flex_center">
-                    <img src={saturn} alt="Saturn" className="home_img_saturn"/>
-                </div>
-            );
-    }
-}
-
-const Rangking =()=>{
+function RankingPage(){
+    const [curPage,setCurPage] = useState(1);
     
-    const arr= range(20);
-    console.log(arr);
-    let final = scoreBoard(curPage,arr);
-    console.log(final);
-    console.log(curPage);
+    const randPlayer=(i)=>{
+        return{
+            rank : i,
+            playerName : "Playername",
+            home : Math.floor(Math.random()*6),
+            point : Math.floor(Math.random()*10000)
+        }
+    }
+
+    const range = (len) =>{
+        const arr=[];
+        for(let i=0;i<len;i++){
+            arr.push(i);
+            arr[i] = randPlayer(i+1);
+        }
+        return arr;
+    }
+    
+    const scoreBoard = (curPage,arr) =>{
+        const borad=[];
+        let len = curPage*10;
+        let start = len-10;
+        let count=0;
+        const lastPage = Math.ceil(arr.length/10);
+        if (curPage == lastPage && arr.length%10!=0){
+            len = ((curPage-1)*10) +arr.length%10;
+            start = len - arr.length%10;
+            console.log(len,start);
+            for(let i = start; i <len; i++ ){
+                borad[count]=arr[i];
+                count++;
+            }
+        }
+        else{
+            for(let i = start; i <len; i++ ){
+                borad[count]=arr[i];
+                count++;
+            }
+        }
+        return borad;
+    }
+
+    const Home = (home) => {
+        switch(home){
+            case 0:
+                return (
+                    <div className="flex_center">
+                        <img src={jupiter} alt="Jupiter" className="home_img"/>
+                    </div>
+                );
+            case 1:
+                return (
+                    <div className="flex_center">
+                            <img src={mars} alt="Mars" className="home_img"/>
+                    </div>
+                );
+            case 2:
+                return (
+                    <div className="flex_center">
+                        <img src={mercury} alt="Mercury" className="home_img"/>
+                    </div>
+                );
+            case 3:
+                return (
+                    <div className="flex_center">
+                        <img src={naptune} alt="Naptune" className="home_img"/>
+                    </div>
+                );
+            case 4:
+                return (
+                    <div className="flex_center">
+                        <img src={pluto} alt="Pluto" className="home_img"/>
+                    </div>
+                );
+            case 5:
+                return (
+                    <div className="flex_center">
+                        <img src={saturn} alt="Saturn" className="home_img_saturn"/>
+                    </div>
+                );
+        }
+    }
 
     const increasePage =() =>{
-        final = scoreBoard(++curPage,arr);
-        console.log(curPage);
-        console.log(final);
+        if(curPage != Math.ceil(startArr.length/10)){
+            console.log(curPage);
+            setCurPage(curPage+1);
+        }
     }
 
     const decreasePage=() =>{
-        curPage--;
-        console.log(curPage);
+        if(curPage != 1){
+            console.log(curPage);
+            setCurPage(curPage-1);
+        }
     }
+
+    const startArr= range(22);
+    console.log(startArr);
+    const showScore = scoreBoard(curPage,startArr);
+    console.log(showScore);
 
     return (
         <div className="scoreBoard bg">
+            hey
             <table >
                 <thead>
                     <tr>
@@ -116,7 +130,7 @@ const Rangking =()=>{
                     </tr>
                 </thead>
                 <tbody>
-                    {final.map(d =>{
+                    {showScore.map(d =>{
                         return (
                             <tr>
                                 <th>{d.rank}</th>
@@ -133,7 +147,7 @@ const Rangking =()=>{
                         <th colSpan={5}>
                             <div className="flex_end gap-x-4">
                                 <button onClick={decreasePage}><FontAwesomeIcon icon={faCircleArrowLeft}></FontAwesomeIcon></button>
-                                <h1>{curPage} / {Math.ceil(arr.length/10)}</h1>
+                                <h1>{curPage} / {Math.ceil(startArr.length/10)}</h1>
                                 <button onClick={increasePage}><FontAwesomeIcon icon={faCircleArrowRight}></FontAwesomeIcon></button>
                             </div>
                         </th>
@@ -142,6 +156,6 @@ const Rangking =()=>{
             </table>
         </div>
     );
-};
+}
 
-export default Rangking;
+export default RankingPage;

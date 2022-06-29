0b71d1b72a46b5329fd5ea31928628b8678eb8df
Batman <sirati10546@gmail.com>
2022-06-29 17:22:58 +0700

    Route Solve

--- src/Route.jsx
+++ src/Route.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
 import Tasks from "./pages/Tasks";
 import Submit from "./pages/Submit";
 import Ranking from "./pages/Ranking";
@@ -11,59 +10,65 @@ import Profile from "./pages/Profile";
 import Guide from "./pages/Guide";
 import Logout from "./components/Logout";
 import ProtectedRoute from "./components/ProtectedRoute";
+
+const HomeRoute = () => {
+  return (
+    <ProtectedRoute>
+      <Home />
+    </ProtectedRoute>
+  );
+};
+
+const TaskRoute = () => {
+  return (
+    <ProtectedRoute>
+      <Tasks />
+    </ProtectedRoute>
+  );
+};
+
+const SubmitRoute = () => {
+  return (
+    <ProtectedRoute>
+      <Submit />
+    </ProtectedRoute>
+  );
+};
+
+const RankingRoute = () => {
+  return (
+    <ProtectedRoute>
+      <Ranking />
+    </ProtectedRoute>
+  );
+};
+
+const GuideRoute = () => {
+  return (
+    <ProtectedRoute>
+      <Guide />
+    </ProtectedRoute>
+  );
+};
+
+const ProfileRoute = () => {
+  return (
+    <ProtectedRoute>
+      <Profile />
+    </ProtectedRoute>
+  );
+};
 const RouteComponent = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/task"
-        element={
-          <ProtectedRoute>
-            <Tasks />
-          </ProtectedRoute>
-        }
-      >
-        <Route
-          path=":id"
-          element={
-            <ProtectedRoute>
-              <Submit />
-            </ProtectedRoute>
-          }
-        />
+      <Route path="/" element={HomeRoute()} />
+      <Route path="/task" element={TaskRoute()}>
+        <Route path=":id" element={SubmitRoute()} />
       </Route>
-      <Route
-        path="/ranking"
-        element={
-          <ProtectedRoute>
-            <Ranking />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/guide"
-        element={
-          <ProtectedRoute>
-            <Guide />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/ranking" element={RankingRoute()} />
+      <Route path="/profile" element={ProfileRoute()} />
+      <Route path="/guide" element={GuideRoute()} />
       <Route path="/logout" element={<Logout />} />
       <Route path="*" element={<Error />} />
     </Routes>
